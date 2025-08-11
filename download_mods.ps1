5b29ebfb5d395be068ea5ee5901946a3bb1fb395
HypoxiE <kosmaer42@gmail.com>
2025-08-11 23:46:48 +0700

    Готова функция скачивания модов

---
+++ download_mods.ps1
@@ -0,0 +1,36 @@
+$fileId = "13im-l6X2j-s5scZZlBvt2DCjYBbk2qI8"
+$baseUrl = "https://drive.google.com/uc?export=download&id=$fileId"
+$outputFile = "$env:TEMP\ruinship_mods.rar"
+
+$response = Invoke-WebRequest -Uri $baseUrl -SessionVariable session
+
+$form = $response.Forms | Where-Object { $_.Id -eq "download-form" }
+
+if ($form) {
+	$queryParams = @{}
+	foreach ($field in $form.Fields.GetEnumerator()) {
+		$queryParams[$field.Key] = $field.Value
+	}
+
+	$uriBuilder = New-Object System.UriBuilder $form.Action
+	$queryString = ($queryParams.GetEnumerator() | ForEach-Object { "$($_.Key)=$($_.Value)" }) -join "&"
+	$uriBuilder.Query = $queryString
+	$downloadUrl = $uriBuilder.Uri.AbsoluteUri
+
+	Write-Host "[INFO] Downloading..."
+	Invoke-WebRequest -Uri $downloadUrl -WebSession $session -OutFile $outputFile
+	Write-Host "[INFO] The file was successfully downloaded with confirmation."
+}
+else {
+	Write-Host "[INFO] Downloading..."
+	Invoke-WebRequest -Uri $baseUrl -OutFile $outputFile
+	Write-Host "[INFO] The file was successfully downloaded directly."
+}
+
+$scriptDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
+$rimworldMods = Resolve-Path (Join-Path $scriptDir "..")
+
+$sourceFolder = Get-ChildItem $outputFile | Select-Object -First 1
+Move-Item -Path $sourceFolder.FullName -Destination $rimworldMods
+
+Write-Host "[INFO] The ruinship_mods.rar file has been installed to the path $rimworldMods. Please extract it to install all the necessary mods." -ForegroundColor Green
--- setup.bat
+++ setup.bat
@@ -1,2 +1,43 @@
 @echo off
-powershell -ExecutionPolicy Bypass -File "%~dp0setup.ps1"
+setlocal enabledelayedexpansion
+chcp 65001
+
+echo Укажите, что вам нужно сделать:
+:ask
+echo 1) Установить моды (они будут только установлены в качестве архива и их надо будет вручную распаковать)
+echo 2) Поставить сценарий
+echo 3) Поставить ModsConfig
+echo 4) Установить всё
+echo 5) Выйти
+
+set /p choice=Введите нужный пункт:
+
+if /i "%choice%"=="1" (
+	powershell -ExecutionPolicy Bypass -File "%~dp0download_mods.ps1"
+	echo Вам что-то ещё?
+	goto ask
+)
+if /i "%choice%"=="2" (
+
+	echo Пока функция не готова. Вам что-то ещё?
+	goto ask
+)
+if /i "%choice%"=="3" (
+
+	echo Пока функция не готова. Вам что-то ещё?
+	goto ask
+)
+if /i "%choice%"=="4" (
+	powershell -ExecutionPolicy Bypass -File "%~dp0download_mods.ps1"
+
+	echo Всё готово^^! Осталось только распаковать моды из архива в папке Mods и можно начинать играть^^!
+	goto end
+)
+if /i "%choice%"=="5" (
+	echo Выхожу...
+	goto end
+)
+echo Варианта %choice% не существует
+goto ask
+
+:end
